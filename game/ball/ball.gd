9dd5eea551e264dd324ec1c6ef09ab69e952d0cc
Jabarton <11004850+5tr1k3r@users.noreply.github.com>
2025-04-28 19:00:36 +0500

    add high velocity hit sound

--- game/ball/ball.gd
+++ game/ball/ball.gd
@@ -6,8 +6,10 @@ signal ball_got_stuck
 @onready var stuck_timer: Timer = $StuckTimer
 @onready var paddle_hit_sound: AudioStreamPlayer3D = $PaddleHitSound
 @onready var wall_hit_sound: AudioStreamPlayer3D = $WallHitSound
+@onready var high_velocity_hit_sound: AudioStreamPlayer3D = $HighVelocityHitSound
 
-var starting_impulse_value: float = 5.0
+const STARTING_IMPULSE_VALUE: float = 5.0
+const HIGH_VELOCITY_THRESHOLD: float = 1.5
 var initial_impulse: Vector3 = Vector3.ZERO
 
 func setup(start_position: Vector3) -> void:
@@ -20,7 +22,7 @@ func setup(start_position: Vector3) -> void:
 	var x_direction: float = [-1.0, 1.0].pick_random()
 	var base_direction := Vector3(x_direction, 0.0, 0.0)
 	var rotated_direction := base_direction.rotated(Vector3.UP, random_angle)
-	initial_impulse = rotated_direction.normalized() * starting_impulse_value
+	initial_impulse = rotated_direction.normalized() * STARTING_IMPULSE_VALUE
 
 func _ready() -> void:
 	stuck_timer.start()
@@ -31,9 +33,13 @@ func _ready() -> void:
 		printerr("Ball added to tree, but initial impulse was not calculated!")
 
 func _on_body_entered(body: Node) -> void:
+	var is_high_velocity_hit := linear_velocity.length() / STARTING_IMPULSE_VALUE > HIGH_VELOCITY_THRESHOLD
+	
 	if body is Paddle:
 		stuck_timer.start()
 		paddle_hit_sound.play()
+		if is_high_velocity_hit:
+			high_velocity_hit_sound.play()
 	elif body is Wall:
 		wall_hit_sound.play()
 
