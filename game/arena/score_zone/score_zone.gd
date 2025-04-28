3ea9f122c0564a583b69a35ef1689f6a735d74f6
Jabarton <11004850+5tr1k3r@users.noreply.github.com>
2025-04-28 20:28:40 +0500

    fade out ball on death

--- game/arena/score_zone/score_zone.gd
+++ game/arena/score_zone/score_zone.gd
@@ -8,4 +8,4 @@ signal player_scored(player_side: Enums.PlayerSide)
 func _on_body_entered(body: Node3D) -> void:
 	if body is Ball:
 		emit_signal("player_scored", scoring_player_side)
-		body.queue_free()
+		body.die()
--- game/ball/ball.gd
+++ game/ball/ball.gd
@@ -7,10 +7,15 @@ signal ball_got_stuck
 @onready var paddle_hit_sound: AudioStreamPlayer3D = $PaddleHitSound
 @onready var wall_hit_sound: AudioStreamPlayer3D = $WallHitSound
 @onready var high_velocity_hit_sound: AudioStreamPlayer3D = $HighVelocityHitSound
+@onready var collision_shape: CollisionShape3D = $CollisionShape3D
+@onready var mesh_instance: MeshInstance3D = $MeshInstance3D
 
 const STARTING_IMPULSE_VALUE: float = 5.0
 const HIGH_VELOCITY_THRESHOLD: float = 1.5
+const DYING_DURATION: float = 0.4
 var initial_impulse: Vector3 = Vector3.ZERO
+var is_dying: bool = false
+var unique_material: StandardMaterial3D
 
 func setup(start_position: Vector3) -> void:
 	position = start_position
@@ -25,12 +30,29 @@ func setup(start_position: Vector3) -> void:
 	initial_impulse = rotated_direction.normalized() * STARTING_IMPULSE_VALUE
 
 func _ready() -> void:
+	# Make the material unique so we can manipulate it and not affect other balls
+	unique_material = mesh_instance.get_active_material(0).duplicate()
+	mesh_instance.set_surface_override_material(0, unique_material)
+	
 	stuck_timer.start()
 	
 	if initial_impulse != Vector3.ZERO:
 		apply_central_impulse(initial_impulse)
 	else:
 		printerr("Ball added to tree, but initial impulse was not calculated!")
+
+func die() -> void:
+	if is_dying:
+		return
+	
+	is_dying = true
+	freeze = true
+	collision_shape.disabled = true
+	
+	var tween := create_tween()
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(unique_material, "albedo_color:a", 0.0, DYING_DURATION)
+	tween.tween_callback(queue_free)
 
 func _on_body_entered(body: Node) -> void:
 	var is_high_velocity_hit := linear_velocity.length() / STARTING_IMPULSE_VALUE > HIGH_VELOCITY_THRESHOLD
@@ -46,4 +68,4 @@ func _on_body_entered(body: Node) -> void:
 func _on_stuck_timer_timeout() -> void:
 	print("BALL STUCK! BALL STUCK!")
 	emit_signal("ball_got_stuck")
-	queue_free()
+	die()
